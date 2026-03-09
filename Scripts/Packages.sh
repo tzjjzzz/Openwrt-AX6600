a94b238ce1f3e4c5d21f2f6317d09e2fc163a176
Ones <96521439+ones20250@users.noreply.github.com>
2026-03-09 04:27:44 +0000

    Update package sources for tailscale and gecoosac

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -60,13 +60,13 @@ UPDATE_PACKAGE "aurora-config" "ones20250/luci-app-aurora-config" "master"
 
 #UPDATE_PACKAGE "mosdns" "sbwml/luci-app-mosdns" "v5" "" "v2dat"
 
-#UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
+#UPDATE_PACKAGE "luci-app-tailscale" "Tokisaki-Galaxy/luci-app-tailscale-community" "master"
 
 #UPDATE_PACKAGE "ddns-go" "sirpdboy/luci-app-ddns-go" "main"
 #UPDATE_PACKAGE "diskman" "lisaac/luci-app-diskman" "master"
 #UPDATE_PACKAGE "easytier" "EasyTier/luci-app-easytier" "main"
 #UPDATE_PACKAGE "fancontrol" "rockjake/luci-app-fancontrol" "main"
-#UPDATE_PACKAGE "gecoosac" "lwb1978/openwrt-gecoosac" "main"
+#UPDATE_PACKAGE "gecoosac" "laipeng668/luci-app-gecoosac" "main"
 #UPDATE_PACKAGE "netspeedtest" "sirpdboy/luci-app-netspeedtest" "master" "" "homebox speedtest"
 #UPDATE_PACKAGE "openlist2" "sbwml/luci-app-openlist2" "main"
 #UPDATE_PACKAGE "partexp" "sirpdboy/luci-app-partexp" "main"
